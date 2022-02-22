2efe83a8f65dee05f13b45cf03f8eb60d042bcea
gib-student <gib19021@byui.edu>
2022-02-22 14:14:38 -0700

    Update

--- analysis.py
+++ analysis.py
@@ -2,4 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib as plt
 
-heights_df = pd.read_excel('data\Heights.xlsx', None, index_col=0, na_values=['NA'])
+heights_df = pd.read_excel('data\Heights.xlsx')
+
+print(heights_df.describe())
